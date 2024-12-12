15648bc0f9780dc2c684c367f914e6208a9592c0
Jorn Peters <jpeters@qti.qualcomm.com>
2024-12-12 16:13:29 +0000

    Add function for random dynamic quantized tensor

--- src/fastforward/quantization/random.py
+++ src/fastforward/quantization/random.py
@@ -9,6 +9,7 @@ from fastforward.quantization import granularity as granularities
 from fastforward.quantized_tensor import QuantizedTensor
 
 from .affine import quantize_by_tile
+from .dynamic import quantize_by_tile as dynamic_quantize_by_tile
 
 
 def random_quantized(
@@ -54,6 +55,44 @@ def random_quantized(
         torch.randn(*shape, device=device),
         scale=scale,
         offset=offset,
+        num_bits=num_bits,
+        tile_size=tile_size,
+        output_dtype=storage_dtype,
+    )
+    if requires_grad:
+        random_tensor.requires_grad_()
+    return random_tensor
+
+
+def random_quantized_dynamic(
+    shape: tuple[int, ...],
+    num_bits: int = 3,
+    requires_grad: bool = False,
+    granularity: granularities.Granularity = granularities.PerTensor(),
+    device: Optional[torch.device | str] = None,
+    storage_dtype: torch.dtype = torch.float32,
+) -> QuantizedTensor:
+    """
+    Generate a random quantized tensor using dynamic quantization
+
+    Args:
+        shape: The shape of the tensor.
+        num_bits: The number of bits for quantization.
+        requires_grad: If True, the tensor requires gradient.
+        granularity: The granularity of quantization.
+        device: The device for the tensor.
+        storage_dtype: The storage data type for the tensor.
+
+    Returns:
+        QuantizedTensor: The generated random quantized tensor.
+
+    Raises:
+        ValueError: If the scale and offset tensors do not have the same number of elements.
+    """
+
+    tile_size = granularity.tile_size(torch.Size(shape))
+    random_tensor = dynamic_quantize_by_tile(
+        torch.randn(*shape, device=device),
         num_bits=num_bits,
         tile_size=tile_size,
         output_dtype=storage_dtype,
