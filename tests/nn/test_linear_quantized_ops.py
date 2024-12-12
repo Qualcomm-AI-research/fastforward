2126889e322327db0de75758d7ebae0c619d3294
Jorn Peters <jpeters@qti.qualcomm.com>
2024-12-12 16:13:49 +0000

    Extend linear_quantized_ops tests to dynamicly quantized tensors

--- tests/nn/test_linear_quantized_ops.py
+++ tests/nn/test_linear_quantized_ops.py
@@ -2,19 +2,28 @@
 # All Rights Reserved.
 
 # pylint: disable=missing-function-docstring
+from typing import Callable
+
 import pytest
 import torch
 
 import fastforward as ff
 
-from fastforward.quantization.random import random_quantized
+# Functions that produce tensors that should be treated as linearly quantized.
+LINEAR_QUANT_GENERATORS = (
+    ff.random.random_quantized,
+    ff.random.random_quantized_dynamic,
+)
 
 
 @pytest.mark.parametrize("data_shape", [(3, 3)])
-def test_getitem_slice_rows(data_shape: tuple[int, int]):
+@pytest.mark.parametrize("quantfunc", LINEAR_QUANT_GENERATORS)
+def test_getitem_slice_rows(
+    data_shape: tuple[int, int], quantfunc: Callable[..., ff.QuantizedTensor]
+):
     # Arrange
     rows, cols = data_shape
-    qx = random_quantized(data_shape)
+    qx = quantfunc(data_shape)
     x = qx.dequantize()
     sliced_rows_x = [x[row] for row in range(rows)]
 
@@ -27,10 +36,13 @@ def test_getitem_slice_rows(data_shape: tuple[int, int]):
 
 
 @pytest.mark.parametrize("data_shape", [(3, 3)])
-def test_getitem_slice_cols(data_shape: tuple[int, int]):
+@pytest.mark.parametrize("quantfunc", LINEAR_QUANT_GENERATORS)
+def test_getitem_slice_cols(
+    data_shape: tuple[int, int], quantfunc: Callable[..., ff.QuantizedTensor]
+):
     # Arrange
     rows, cols = data_shape
-    qx = random_quantized(data_shape)
+    qx = quantfunc(data_shape)
     x = qx.dequantize()
     sliced_cols_x = [x[:, col] for col in range(cols)]
 
@@ -43,11 +55,16 @@ def test_getitem_slice_cols(data_shape: tuple[int, int]):
 
 
 @pytest.mark.parametrize(
-    "data_shape", [(3,), (3, 3), (3, 3, 3), (3, 3, 3, 3)], ids=["1D", "2D", "3D", "4D"]
+    "data_shape",
+    [(3,), (3, 3), (3, 3, 3), (3, 3, 3, 3)],
+    ids=["1D", "2D", "3D", "4D"],
 )
-def test_getitem_slice_multi_dims(data_shape: tuple[int]):
+@pytest.mark.parametrize("quantfunc", LINEAR_QUANT_GENERATORS)
+def test_getitem_slice_multi_dims(
+    data_shape: tuple[int], quantfunc: Callable[..., ff.QuantizedTensor]
+):
     # Arrange
-    qx = random_quantized(data_shape)
+    qx = quantfunc(data_shape)
     x = qx.dequantize()
 
     for dim in range(len(data_shape)):
@@ -62,14 +79,32 @@ def test_getitem_slice_multi_dims(data_shape: tuple[int]):
             assert torch.all(sliced_qx.dequantize() == sliced_x)
 
 
-def test_scalar_multiply():
-    qx = random_quantized((3, 3), requires_grad=True)
+@pytest.mark.parametrize("quantfunc", LINEAR_QUANT_GENERATORS)
+def test_scalar_multiply(quantfunc: Callable[..., ff.QuantizedTensor]):
+    # Given: a per-tensor quantized tensor and a scalar scale_factor
+    qx = quantfunc((3, 3), requires_grad=True)
+    scale = qx.quant_args().scale
     scale_factor = 0.25
-    with ff.set_strict_quantization(False):
-        observed = ff.nn.functional.mul(qx, scale_factor)
 
+    # When: a quantized tensor is multiplied by a scalar scale factor
+    observed = ff.nn.functional.mul(qx, scale_factor)
+
+    # Then: the result is a quantized tensor
+    assert isinstance(observed, ff.QuantizedTensor)
+
+    # Then: only the scale parameter is affected
+    assert observed.quant_args().scale == scale * scale_factor
+
+    # Then: the scale_factor has effect on the dequantized tensor
     expected = qx.dequantize() * scale_factor
     torch.testing.assert_close(expected, observed.dequantize())
+
+    # When: computing the backward pass
+    with ff.strict_quantization(False):
+        expected.sum().backward()
+
+    # Then: the scale correctly affects the gradient
+    assert (qx.grad == scale_factor).all()  # type: ignore[union-attr]
 
 
 @pytest.mark.parametrize(
@@ -85,10 +120,11 @@ def test_scalar_multiply():
         ff.PerTile((2, 16)),
     ],
 )
-def test_unsqueeze(granularity):
+@pytest.mark.parametrize("quantfunc", LINEAR_QUANT_GENERATORS)
+def test_unsqueeze(granularity, quantfunc: Callable[..., ff.QuantizedTensor]):
     num_bits = 4
 
-    qx = random_quantized(
+    qx = quantfunc(
         (16, 16),
         num_bits=num_bits,
         granularity=granularity,
@@ -100,9 +136,18 @@ def test_unsqueeze(granularity):
         torch.testing.assert_close(actual, expected)
 
 
-def test_expand():
+@pytest.mark.parametrize("quantfunc", LINEAR_QUANT_GENERATORS)
+def test_expand(quantfunc: Callable[..., ff.QuantizedTensor]):
+    """
+    Test `torch.expand/torch.Tensor.expand` implementation for quantized
+    tensors for which the following holds:
+
+        - Per-Tensor quantized
+        - Either dynamically or staticly quantized
+            (i.e., fastforward.affine or fastforward.dynamic)
+    """
     # Given: A quantized and non-quantized tensor that represent the same values
-    quantized_data = ff.quantization.random.random_quantized((3, 3))
+    quantized_data = quantfunc((3, 3))
     data = quantized_data.dequantize()
 
     # When: Expand the second dimension of both tensors
