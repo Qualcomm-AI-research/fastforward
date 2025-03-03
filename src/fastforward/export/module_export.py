b3432b563d132b20a92766b3e6ecacc2614fd139
Spyridon Stasis <sstasis@qti.qualcomm.com>
2025-03-03 11:42:00 +0000

    Remove staticmethod for tensor dequantization

--- src/fastforward/export/module_export.py
+++ src/fastforward/export/module_export.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2024 Qualcomm Technologies, Inc.
 # All Rights Reserved.
 
-import collections
 import json
 import pathlib
 import pickle
@@ -11,6 +10,8 @@ from typing import Any
 import onnxruntime  # type: ignore[import-untyped]
 import torch
 
+from typing_extensions import override
+
 from fastforward.export import export
 from fastforward.export._export_helpers import QuantParametersDict, create_qnn_encoding_entry
 from fastforward.mpath._search import MPathCollection
@@ -18,7 +19,7 @@ from fastforward.quantization.affine.function import StaticAffineQuantParams
 from fastforward.quantized_tensor import QuantizedTensor
 
 
-class ModuleIOCaptureRecorder:
+class ModuleIORecorder:
     """Provides functionality for logging inputs/outputs/kwargs."""
 
     def __init__(self, module: torch.nn.Module, module_name: str):
@@ -45,17 +46,18 @@ class ModuleIOCaptureRecorder:
         self.kwargs = kwargs
         self.output = output_ if isinstance(output_, tuple) else (output_,)
 
-        self.input, self.input_quantizer_settings = self.maybe_dequantize_tensors(self.input)
-        self.output, self.output_quantizer_settings = self.maybe_dequantize_tensors(self.output)
+        self.input, self.input_quantizer_settings = maybe_dequantize_tensors(self.input)
+        self.output, self.output_quantizer_settings = maybe_dequantize_tensors(self.output)
 
+    @override
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__} for module {self.module_name}:\n{self.module}"
+        return f"{self.__class__.__name__} for module {self.module_name}"
 
     def attach(self) -> None:
         """Assign handle to object instance."""
         if self.handle is not None:
-            msg = f"Handle for module: {self} is already attached. Cannot attach a new one."
-            raise AttributeError(msg)
+            msg = f"Handle for {self} is already attached. Cannot attach a new one."
+            raise RuntimeError(msg)
         self.handle = self.module.register_forward_hook(hook=self, with_kwargs=True)
 
     def detach(self) -> None:
@@ -64,48 +66,6 @@ class ModuleIOCaptureRecorder:
             msg = f"There is no handle to detach."
             raise AttributeError(msg)
         self.handle.remove()
-
-    @staticmethod
-    def maybe_dequantize_tensors(
-        tensors: tuple[torch.Tensor],
-    ) -> tuple[tuple[torch.Tensor, ...], tuple[QuantParametersDict, ...]]:
-        """Dequantizes tensors.
-
-        The output tensors of quantized modules will usually be returned
-        as `QuantizedTensor`s. As these are custom tensors they cannot be
-        used in that form for exporting, and need to be dequantized. This
-        function performs this dequantization in the case a `QuantizedTensor`
-        is found on the input/output module capture, and it also stores
-        its quantization settings, so these can be appended to the encodings
-        file.
-
-        Args:
-            tensors: A tuple of tensors, where some of these may be
-                `QuantizedTensor`s.
-
-        Returns:
-            The (maybe) dequantized tensors, and the quantizer settings for
-            each of those.
-        """
-        output_tensors: list[torch.Tensor] = []
-        quantizer_settings: list[QuantParametersDict] = []
-
-        for tensor in tensors:
-            if isinstance(tensor, QuantizedTensor):
-                quant_args: StaticAffineQuantParams = tensor.quant_args()  # type: ignore[assignment]
-                scale, offset, num_bits = quant_args.scale, quant_args.offset, quant_args.num_bits
-                tensor_quant_args: QuantParametersDict = {
-                    "scale": scale,
-                    "offset": offset,
-                    "num_bits": num_bits,
-                }
-
-                quantizer_settings.append(tensor_quant_args)
-                output_tensors.append(tensor.dequantize())
-            else:
-                output_tensors.append(tensor)
-
-        return tuple(output_tensors), tuple(quantizer_settings)
 
     def store_io_as_dict(self, location: pathlib.Path) -> None:
         """Store inputs/outputs/kwargs as a pickle dictionary."""
@@ -168,9 +128,9 @@ def export_modules(
     else:
         modules = {model_name: module_or_module_collection}
 
-    module_io_recorders = []
+    module_io_recorders: list[ModuleIORecorder] = []
     for module_name, module in modules.items():
-        module_io_recorder = ModuleIOCaptureRecorder(module, module_name)
+        module_io_recorder = ModuleIORecorder(module, module_name)
         module_io_recorders.append(module_io_recorder)
         module_io_recorder.attach()
 
@@ -217,7 +177,7 @@ def maybe_extend_encodings_file(
 
     As detailed in the `export_modules` function docstring there are
     cases when quantization is implicit (the tensor passed to the
-    operation is already quantized) and it cannot be captured in the
+    operation is already quantized) and it was not captured in the
     encodings file. This function takes a dictionary of quantization
     settings and appends them to the encodings file of the given module.
 
@@ -235,8 +195,13 @@ def maybe_extend_encodings_file(
     with open(encodings_file_location) as fp:
         encodings_dictionary = json.load(fp)
 
+    # The existing encodings dictionary (only its `activation_encodings` section)
+    # might need to be overwritten.
     activation_encodings_dictionary = encodings_dictionary["activation_encodings"]
 
+    # In order to associate potential inputs with quantizer settings we need
+    # to retrieve the graph input names. This can be done through the onnxruntime
+    # library.
     ort_session = onnxruntime.InferenceSession(
         onnx_artifact_location, providers=["CPUExecutionProvider"]
     )
@@ -244,24 +209,76 @@ def maybe_extend_encodings_file(
     quantizer_input_settings = quantizer_settings["input"]
     ort_session_inputs = ort_session.get_inputs()
 
-    for ort_input_, quant_settings in zip(ort_session_inputs, quantizer_input_settings):
-        ort_input_name = ort_input_.name
+    # Here we iterate through the quantizer input settings and in the case that
+    # an input does not appear on the original encodings file then we create
+    # a new entry for it.
+    # NB: We cannot retrieve the input's name from the QuantizedTensor, because
+    # we are operating at the module and not graph level. However, we can consider
+    # that the inputs will appear in the same sequence in both graph and module.
+    for input_idx, quant_settings in enumerate(quantizer_input_settings):
+        ort_input_name = ort_session_inputs[input_idx].name
 
         if ort_input_name not in activation_encodings_dictionary:
             qnn_encoding_entry = create_qnn_encoding_entry(quant_settings)
             activation_encodings_dictionary[ort_input_name] = qnn_encoding_entry
 
+    # The same process detailed for appending input encodings to the encodings
+    # dictionary is mirrored for the output encodings.
     quantizer_output_settings = quantizer_settings["output"]
     ort_session_outputs = ort_session.get_outputs()
 
-    for ort_output_, quant_settings in zip(ort_session_outputs, quantizer_output_settings):
-        ort_output_name = ort_output_.name
+    for output_idx, quant_settings in enumerate(quantizer_output_settings):
+        ort_output_name = ort_session_outputs[output_idx].name
 
         if ort_output_name not in activation_encodings_dictionary:
             qnn_encoding_entry = create_qnn_encoding_entry(quant_settings)
             activation_encodings_dictionary[ort_output_name] = qnn_encoding_entry
 
+    # We overwrite the `activation_encodings` of the original encodings dictionary
+    # so it includes any non-explicitly quantized inputs and store the updated version.
     encodings_dictionary["activation_encodings"] = activation_encodings_dictionary
 
     with open(encodings_file_location, "w") as fp:
         json.dump(encodings_dictionary, fp, indent=4)
+
+
+def maybe_dequantize_tensors(
+    tensors: tuple[torch.Tensor],
+) -> tuple[tuple[torch.Tensor, ...], tuple[QuantParametersDict, ...]]:
+    """Dequantizes tensors.
+
+    The output tensors of quantized modules will usually be returned
+    as `QuantizedTensor`s. As these are custom tensors they cannot be
+    used in that form for exporting, and need to be dequantized. This
+    function performs this dequantization in the case a `QuantizedTensor`
+    is found on the input/output module capture, and it also stores
+    its quantization settings, so these can be appended to the encodings
+    file.
+
+    Args:
+        tensors: A tuple of tensors, where some of these may be
+            `QuantizedTensor`s.
+
+    Returns:
+        The (maybe) dequantized tensors, and the quantizer settings for
+        each of those.
+    """
+    output_tensors: list[torch.Tensor] = []
+    quantizer_settings: list[QuantParametersDict] = []
+
+    for tensor in tensors:
+        if isinstance(tensor, QuantizedTensor):
+            quant_args: StaticAffineQuantParams = tensor.quant_args()  # type: ignore[assignment]
+            scale, offset, num_bits = quant_args.scale, quant_args.offset, quant_args.num_bits
+            tensor_quant_args: QuantParametersDict = {
+                "scale": scale,
+                "offset": offset,
+                "num_bits": num_bits,
+            }
+
+            quantizer_settings.append(tensor_quant_args)
+            output_tensors.append(tensor.dequantize())
+        else:
+            output_tensors.append(tensor)
+
+    return tuple(output_tensors), tuple(quantizer_settings)
