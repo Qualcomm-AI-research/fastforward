72d4b090b3467ac3715a4fbbc6fef237506c5029
Jorn Peters <jpeters@qti.qualcomm.com>
2025-07-14 11:47:48 +0000

    Add test for raise statement in autoquant input

---
+++ tests/autoquant/test_autoquant_functions.py
@@ -0,0 +1,43 @@
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
+from typing import Callable
+
+import fastforward as ff
+import pytest
+
+
+def autoquant_case(
+    input: str, expected: str, as_module: bool = False, mark_slow: bool = True
+) -> Callable[[], None]:
+    def test_function() -> None:
+        ff.testing.autoquant.assert_autoquantize_result(input, expected, as_module)
+
+    if mark_slow:
+        test_function = pytest.mark.slow(test_function)
+
+    return test_function
+
+
+test_raise = autoquant_case(
+    # Autoquant for `raise` behaves similarly to `return`. I.e., no new
+    # quantizer should be introduced after 'resetting the quantization status'.
+    # In the case below, the assignment to `x` in the if branch is not 'live'
+    # after the raise statement. Hence, no quantization calls must be
+    # introduced due to the multiplication in the final return statement.
+    input="""
+    def my_function(self, x: torch.Tensor) -> torch.Tensor:
+        if True:
+            x = some_function(x) # reset quantization status
+            raise ValueError("msg")
+        return x * x
+    """,
+    expected="""
+    def my_function(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.quantizer_x(x)
+        if True:
+            x = some_function(x) # reset quantization status
+            raise ValueError("msg")
+        return fastforward.nn.functional.mul(x, x, output_quantizer=self.quantizer_mul)
+    """,
+)
