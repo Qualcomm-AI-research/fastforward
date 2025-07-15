58c5932a229faa14c53ffaa79dcbade53c258176
Sönke Behrends <sbehrend@qti.qualcomm.com>
2025-07-15 14:49:58 +0000

    refactor: remove CFG-related code
    The CFG-based approach is no longer in use in autoquant.

--- src/fastforward/_autoquant/cfg/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
-# SPDX-License-Identifier: BSD-3-Clause-Clear
-
-from .construct import construct as construct
-from .reconstruct import reconstruct as reconstruct
